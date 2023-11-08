d6762fda64676fd30ee2783f9853b25bcff8a197
dlgusdk <83761937+dlgusdk@users.noreply.github.com>
2023-11-08 12:36:51 +0900

    feat:댓글 CRUD 추가
    CommentEntity, CommentController, CommentRepository에 CRUD 기능 추가

--- src/main/java/com/example/beproject/domain/comment/Comment.java
+++ src/main/java/com/example/beproject/domain/comment/Comment.java
@@ -1,5 +1,9 @@
 package com.example.beproject.domain.comment;
 
+import com.example.beproject.domain.member.Member;
+import com.example.beproject.entity.comment.CommentEntity;
+import com.example.beproject.entity.member.MemberEntity;
+import com.example.beproject.entity.post.PostEntity;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,7 +11,6 @@ import lombok.Getter;
 public class Comment {
 
     private final Long id;
-
     //대댓글 아이디
     private final Long write;
 
@@ -16,18 +19,32 @@ public class Comment {
     private final Long orgid;
 
     private final Long subid;
+
     private final String status;
+
+    private final PostEntity post;
 
 
     @Builder
-    public Comment(Long id, Long write, String contents, Long orgid, Long subid, String status) {
+    public Comment(Long id, Long write, String contents, Long orgid, Long subid, String status, PostEntity post) {
         this.id = id;
         this.write = write;
         this.contents = contents;
         this.orgid = orgid;
         this.subid = subid;
         this.status = status;
-
+        this.post  = post;
     }
 
+    public static Comment from(CommentEntity comment) {
+        return Comment.builder()
+                .id(comment.getId())
+                .write(comment.getWrite())
+                .contents(comment.getContents())
+                .orgid(comment.getOrgid())
+                .subid(comment.getSubid())
+                .status(comment.getStatus())
+                .post(comment.getPost())
+                .build();
+    }// CommentEntity를 Comment로
 }
